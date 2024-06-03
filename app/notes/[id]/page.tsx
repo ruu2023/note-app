44762a4420b5058580304a0f6355c553b3bf872d
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-06-04 06:28:23 +0900

    done

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Note from "./Note";
+import { getDetailNote, getNotesList } from "@/lib/client";
+
+export async function generateStaticParams() {
+
+  const { contents } = await getNotesList();
+
+  return contents.map((note) => ({
+    id: note.id,
+  }));
+}
+
+export default async function Page({params}: {params: {id: string}}) {
+
+  const note = await getDetailNote(params.id);
+
+  return (
+    <main className="mx-2 sm:mx-4">
+      <Link href="/notes">← back</Link>
+      <h2 className="my-4 text-gray-400 text-xs">View Note</h2>
+      <Note note={note} />
+    </main>
+  )
+}
